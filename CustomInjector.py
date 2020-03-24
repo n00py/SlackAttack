b7a9411cf1a6c87938c9129d0b965098b43da35c
Evan Edwards <39352103+ejedev@users.noreply.github.com>
2020-03-24 14:35:58 -0700

    Updated Custom Injector for Slack 4.4.

--- CustomInjector.py
+++ CustomInjector.py
@@ -1,7 +1,7 @@
 from asarPy import extract_asar
 from os import listdir
 import os.path
-print("Welcome to the SlackAttack v1.1 custom injector!\nPlease enter the location of the JS code you would like to inject:\n")
+print("Welcome to the SlackAttack v1.2 custom injector!\nPlease enter the location of the JS code you would like to inject:\n")
 location = input()
 with open(location, 'r') as jsfile:
   js = "\n" + jsfile.read()
@@ -17,7 +17,7 @@ appFile = verDir + "\\app-" + maxVer + "\\resources\\app.asar"
 extractFolder =  verDir + "\\app-" + maxVer + "\\resources\\"
 os.system("taskkill /IM slack.exe /F")
 extract_asar(appFile, extractFolder + "app")
-file = open(extractFolder + "app\\" + "dist\\main-preload-entry-point.bundle.js", 'a')
+file = open(extractFolder + "app\\" + "dist\\preload.bundle.js", 'a')
 file.write(js)
 file.close()
 os.system("del /f " + appFile)
